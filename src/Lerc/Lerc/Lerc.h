f5a2e13fac728b1939012515cab00b453f1fc96d
tmaurer3 <tmaurer@esri.com>
2015-12-10 16:39:06 -0800

    updated legal headers in all src files with better link to license & notice files

--- src/Lerc/Lerc/Lerc.h
+++ src/Lerc/Lerc/Lerc.h
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,8 +16,7 @@ limitations under the License.
 A local copy of the license and additional notices are located with the
 source distribution at:
 
-  http://www.github.com/esri/lerc/LICENSE
-  http://www.github.com/esri/lerc/NOTICE
+http://github.com/Esri/lerc/
 
 Contributors:  Thomas Maurer
 */
