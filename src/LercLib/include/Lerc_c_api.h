0e2d682d6ee6ce9b8341e0f193b8a75ae7f78521
thom6262 <tmaurer@esri.com>
2021-04-21 12:10:40 -0700

    kmilos/last_tweaks

--- src/LercLib/include/Lerc_c_api.h
+++ src/LercLib/include/Lerc_c_api.h
@@ -45,7 +45,7 @@ extern "C" {
 #define LERC_AT_LEAST_VERSION(maj,min,patch) \
     (LERC_VERSION_NUMBER >= LERC_COMPUTE_VERSION(maj,min,patch))
 
-#ifdef _WIN32
+#if defined _WIN32 || defined __CYGWIN__
 #  ifdef LERC_EXPORTS
 #    define LERCDLL_API __declspec(dllexport)
 #  else
