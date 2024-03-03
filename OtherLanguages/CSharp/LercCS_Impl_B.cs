3c4bdb2334059acb0de244b2262dfc474e6974aa
Ákos Halmai <halmaia@gmail.com>
2024-03-03 19:51:34 +0100

    Missing getDataRangesAdded & bugfixes

--- OtherLanguages/CSharp/LercCS_Impl_B.cs
+++ OtherLanguages/CSharp/LercCS_Impl_B.cs
@@ -1,12 +1,12 @@
 // Alternative implementation for Lerc Encoding/Decoding in C#.
 // By Ákos Halmai 2024.
 
-
 using System;
 using System.IO;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
 
 namespace LercNS
 {
@@ -83,11 +83,10 @@ namespace LercNS
             {
                 uint decodedRasterDataSize = NDepth * NCols * NRows * NBands * (DataType switch
                 {
-                    DataType.Byte or DataType.SByte => sizeof(byte),
-                    DataType.Short or DataType.UShort => sizeof(short),
-                    DataType.Int or DataType.UInt => sizeof(int),
-                    DataType.Float => sizeof(float),
-                    DataType.Double => sizeof(double),
+                    DataType.Byte or DataType.SByte => sizeof(byte), // 1 byte
+                    DataType.Short or DataType.UShort => sizeof(short), // 2 bytes
+                    DataType.Int or DataType.UInt or DataType.Float => sizeof(int), // 4 bytes
+                    DataType.Double => sizeof(double), // 8 bytes
                     _ => ThrowUnsupportedDataType()
                 });
 
@@ -183,21 +182,21 @@ namespace LercNS
         }
 
         /// <summary>
-        /// Encodes the provided ReadOnlySpan<T>. T can be byte, sbyte, short, ushort, int, uint, 
+        /// Encodes the provided ReadOnlySpan of T. T can be byte, sbyte, short, ushort, int, uint, 
         /// float and double.
         /// </summary>
         /// <typeparam name="T">T can be byte, sbyte, short, ushort, int, uint, float and double.</typeparam>
         /// <param name="rasterData">Data to compress, stored as vector.</param>
-        /// <param name="nDepth">Number of multidimensional planes. Use 1 for common rasters.</param>
+        /// <param name="nDepth">Number of values per pixel.</param>
         /// <param name="nCols">Number of columns.</param>
         /// <param name="nRows">Number of rows.</param>
         /// <param name="nBands">Number of bands.</param>
         /// <param name="maskType">Type of masking.</param>
         /// <param name="maxZErr">Maximal error of compression.</param>
         /// <param name="pixelMasks">Byte mask of valid pixels.</param>
-        /// <param name="encodedLercBlobBuffer">Preallocated buffer for output. If you leave it empty/default,
-        /// if will be allocated & filled automatically.</param>
-        /// <returns>Compressed bytes as Span<byte>. If outputBuffer is provided, no allocation occur.</returns>
+        /// <param name="encodedLercBlobBuffer">Pre-allocated buffer for output. If you leave it empty/default,
+        /// if will be allocated and filled automatically.</param>
+        /// <returns>Compressed bytes as Span of byte. If outputBuffer is provided, no allocation occur.</returns>
         [SkipLocalsInit]
         public static unsafe Span<byte> Encode<T>(ReadOnlySpan<T> rasterData,
                                                         int nDepth,
@@ -209,12 +208,12 @@ namespace LercNS
                                                         ReadOnlySpan<byte> pixelMasks = default,
                                                         Span<byte> encodedLercBlobBuffer = default) where T : unmanaged
         {
-            DataType dataType = GetLercDataType<T>();
-            int nMasks = MaskTypeToNMask(nBands, maskType);
-
             fixed (T* pData = rasterData)
             fixed (byte* pValidBytes = pixelMasks)
             {
+                DataType dataType = GetLercDataType<T>();
+                int nMasks = MaskTypeToNMask(nBands, maskType);
+
                 int bufferLength = encodedLercBlobBuffer.Length;
                 if (bufferLength is 0)
                 {
@@ -269,15 +268,40 @@ namespace LercNS
         }
 
         [SkipLocalsInit]
+        public static unsafe void GetDataRanges(ReadOnlySpan<byte> encodedLercBlob,
+                                                      int nDepth,
+                                                      int nBands,
+                                                      ref Span<double> MinValues,
+                                                      ref Span<double> MaxValues)
+        {
+            int expectedLength = nDepth * nBands;
+
+            if (MinValues.Length < expectedLength)
+                MinValues = GC.AllocateUninitializedArray<double>(expectedLength);
+
+            if (MaxValues.Length < expectedLength)
+                MaxValues = GC.AllocateUninitializedArray<double>(expectedLength);
+
+            fixed (double* pMins = MinValues)
+            fixed (double* pMaxs = MaxValues)
+            fixed (byte* pLercBlob = encodedLercBlob)
+                LercAPICall(lerc_getDataRanges(pLercBlob,
+                    (uint)encodedLercBlob.Length,
+                    nDepth, nBands,
+                    pMins, pMaxs));
+        }
+
+        [SkipLocalsInit]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe Span<byte> Decode(ReadOnlySpan<byte> elcodedLercBlob,
                                                 out LercBlobInfo lercBlobInfo,
                                                 out DataRangeInfo dataRangeInfo,
                                                 ref Span<byte> pixelMasks,
-                                                Span<byte> rasterDataBuffer = default)
-        {
-            lercBlobInfo = GetLercBlobInfo(elcodedLercBlob, out dataRangeInfo);
-            return Decode(elcodedLercBlob, lercBlobInfo, ref pixelMasks, rasterDataBuffer);
-        }
+                                                Span<byte> rasterDataBuffer = default) =>
+            Decode(elcodedLercBlob,
+                   lercBlobInfo = GetLercBlobInfo(elcodedLercBlob, out dataRangeInfo),
+                   ref pixelMasks,
+                   rasterDataBuffer);
 
         [SkipLocalsInit]
         public static unsafe Span<byte> Decode(ReadOnlySpan<byte> encodedLercBlob,
@@ -291,8 +315,9 @@ namespace LercNS
             if (rasterDataBuffer.IsEmpty)
                 rasterDataBuffer = GC.AllocateUninitializedArray<byte>(decodedRasterDataSize);
 
-            if (lercBlobInfo.NMasks is not 0 && pixelMasks.IsEmpty)
-                pixelMasks = GC.AllocateUninitializedArray<byte>((int)lercBlobInfo.PixelMasksLength);
+            int pixelMaskSize = (int)lercBlobInfo.PixelMasksLength;
+            if (lercBlobInfo.NMasks is not 0 && (pixelMasks.Length < pixelMaskSize))
+                pixelMasks = GC.AllocateUninitializedArray<byte>(pixelMaskSize);
 
             fixed (byte* pLercBlob = encodedLercBlob)
             fixed (byte* pData = rasterDataBuffer)
@@ -388,16 +413,20 @@ namespace LercNS
             get => File.Exists(LercDLL);
         }
 
+        // SYSLIB1054 warnings suppressed, because there is no marshaling involved, no marshaling code needed.
+#pragma warning disable SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
+
         [DllImport(LercDLL, BestFitMapping = false,
                    CallingConvention = CallingConvention.StdCall,
                    ExactSpelling = true)]
+
         private static extern unsafe ErrorCode lerc_computeCompressedSize(
                                           [DisallowNull][NotNull] void* pData,       // raw image data, row by row, band by band
                                           DataType dataType, // char = 0, uchar = 1, short = 2, ushort = 3, int = 4, uint = 5, float = 6, double = 7
-                                          int nDepth,        // number of values per pixel (e.g., 3 for RGB, data is stored as [RGB, RGB, ...])
+                                          int nDepth,        // number of values per pixel (e.g., 3 for RGB, data is stored as [RGB, RGB, …])
                                           int nCols,         // number of columns
                                           int nRows,         // number of rows
-                                          int nBands,        // number of bands (e.g., 3 for [RRRR ..., GGGG ..., BBBB ...])
+                                          int nBands,        // number of bands (e.g., 3 for [RRRR …, GGGG …, BBBB …])
                                           int nMasks,        // 0 - all valid, 1 - same mask for all bands, nBands - masks can differ between bands
                                           byte* pValidBytes, // nullptr if all pixels are valid; otherwise 1 byte per pixel (1 = valid, 0 = invalid)
                                           double maxZErr,    // max coding error per pixel, defines the precision
@@ -409,10 +438,10 @@ namespace LercNS
         private static extern unsafe ErrorCode lerc_encode(
                                           [DisallowNull][NotNull] void* pData, // raw image data, row by row, band by band
                                           DataType dataType,                   // char = 0, uchar = 1, short = 2, ushort = 3, int = 4, uint = 5, float = 6, double = 7
-                                          int nDepth,                          // number of values per pixel (e.g., 3 for RGB, data is stored as [RGB, RGB, ...])
+                                          int nDepth,                          // number of values per pixel (e.g., 3 for RGB, data is stored as [RGB, RGB, …])
                                           int nCols,                           // number of columns
                                           int nRows,                           // number of rows
-                                          int nBands,                          // number of bands (e.g., 3 for [RRRR ..., GGGG ..., BBBB ...])
+                                          int nBands,                          // number of bands (e.g., 3 for [RRRR …, GGGG …, BBBB …])
                                           int nMasks,                          // 0 - all valid, 1 - same mask for all bands, nBands - masks can differ between bands
                                           byte* pValidBytes,                   // nullptr if all pixels are valid; otherwise 1 byte per pixel (1 = valid, 0 = invalid)
                                           double maxZErr,                      // max coding error per pixel, defines the precision
@@ -432,6 +461,17 @@ namespace LercNS
                                            [ConstantExpected(Max = DataRangeInfo.Count, Min = DataRangeInfo.Count)] int dataRangeFieldCount = DataRangeInfo.Count);           // number of elements of dataRangeArray
 
         [DllImport(LercDLL, BestFitMapping = false,
+           CallingConvention = CallingConvention.StdCall,
+           ExactSpelling = true)]
+        private static extern unsafe ErrorCode lerc_getDataRanges(
+                           [DisallowNull][NotNull] byte* pLercBlob, // Lerc blob to decode
+                           uint blobSize,             // blob size in bytes
+                           int nDepth,                // number of values per pixel (e.g., 3 for RGB, data is stored as [RGB, RGB, …])
+                           int nBands,                // number of bands (e.g., 3 for [RRRR …, GGGG …, BBBB …])
+                           [DisallowNull][NotNull] double* pMins,  // outgoing minima per dimension and band 
+                           [DisallowNull][NotNull] double* pMaxs); // outgoing maxima per dimension and band
+
+        [DllImport(LercDLL, BestFitMapping = false,
                    CallingConvention = CallingConvention.StdCall,
                    ExactSpelling = true)]
         private static extern unsafe ErrorCode lerc_decode(
@@ -439,13 +479,14 @@ namespace LercNS
                                    uint blobSize,             // blob size in bytes
                                    int nMasks,                // 0, 1, or nBands; return as many masks in the next array
                                    byte* pValidBytes,         // gets filled if not nullptr, even if all valid
-                                   int nDepth,                // number of values per pixel (e.g., 3 for RGB, data is stored as [RGB, RGB, ...])
+                                   int nDepth,                // number of values per pixel (e.g., 3 for RGB, data is stored as [RGB, RGB, …])
                                    int nCols,                 // number of columns
                                    int nRows,                 // number of rows
-                                   int nBands,                // number of bands (e.g., 3 for [RRRR ..., GGGG ..., BBBB ...])
+                                   int nBands,                // number of bands (e.g., 3 for [RRRR …, GGGG …, BBBB …])
                                    DataType dataType,         // char = 0, uchar = 1, short = 2, ushort = 3, int = 4, uint = 5, float = 6, double = 7
                                    [DisallowNull][NotNull] void* pData); // outgoing data array
 
+#pragma warning restore SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
         #endregion "Native P/Invoke"
     };
 }
